13193abda654adeba7ed89a61598d8ce163abfbf
rubenxl9 <61393059+rubenxl9@users.noreply.github.com>
2023-06-01 10:33:34 +0200

    Backend kassa

--- FotoKiosk/CheckoutPage.xaml.cs
+++ FotoKiosk/CheckoutPage.xaml.cs
@@ -30,8 +30,48 @@ namespace FotoKiosk
 
         private void AddEl_Click(object sender, RoutedEventArgs e)
         {
+            if (Product.SelectedItem is ComboBoxItem selectedProductItem)
+            {
+                string selectedProduct = selectedProductItem.Content.ToString();
 
+                if (int.TryParse(AantalProduct.Text, out int quantity))
+                {
+                    double productPrice = GetProductPrice(selectedProduct);
+                    double totalAmount = productPrice * quantity;
+                    EindBedrag.Text = totalAmount.ToString("C");
+                }
+                else
+                {
+                    // Quantity is not a valid integer
+                    // Handle the error accordingly
+                }
+            }
+            else
+            {
+                // No product is selected
+                // Handle the error accordingly
+            }
         }
+
+        private double GetProductPrice(string product)
+        {
+            Dictionary<string, double> productPrices = new Dictionary<string, double>()
+    {
+        { "Foto 10x15", 2.55 },
+        { "Foto 20x30", 4.95 },
+        { "Mok met foto", 9.95 },
+        { "Sleutelhanger", 6.12 },
+        { "T-shirt", 11.99 }
+    };
+
+            if (productPrices.ContainsKey(product))
+            {
+                return productPrices[product];
+            }
+
+            return 0.0; // Default to 0 if the product is not found
+        }
+
 
         private void ResetEl_Click(object sender, RoutedEventArgs e)
         {
