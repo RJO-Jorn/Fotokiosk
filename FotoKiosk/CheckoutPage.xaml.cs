aefb70df400ebad5f17e53f2f665b218cd9d2c57
rubenxl9 <61393059+rubenxl9@users.noreply.github.com>
2023-06-09 10:29:22 +0200

    Checkout page fully fixed

--- FotoKiosk/CheckoutPage.xaml.cs
+++ FotoKiosk/CheckoutPage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -27,7 +28,7 @@ namespace FotoKiosk
             this.InitializeComponent();
             Instance = this;
         }
-
+        double totalPrice = 0.0;
         private void AddEl_Click(object sender, RoutedEventArgs e)
         {
             if (Product.SelectedItem is ComboBoxItem selectedProductItem)
@@ -37,9 +38,12 @@ namespace FotoKiosk
                 if (int.TryParse(AantalProduct.Text, out int quantity))
                 {
                     double productPrice = GetProductPrice(selectedProduct);
-                    double totalAmount = productPrice * quantity;
-                    double totalPrice = totalAmount + totalPrice;
-                    EindBedrag.Text = totalPrice.ToString("C");
+                    double totalAmount = productPrice * quantity;                  
+                    totalPrice += totalAmount;
+
+                    EindBedrag.Text = "Eindbedrag: \n" + totalPrice.ToString("C");
+
+
 
                     ListBoxItem receiptItem = new ListBoxItem();
                     receiptItem.Content = $"{quantity}x {selectedProduct} - {totalAmount:C}";
@@ -78,6 +82,7 @@ namespace FotoKiosk
             EindBedrag.Text = string.Empty;
             KassaBon.Items.Clear();
             AantalProduct.Text = string.Empty;
+            totalPrice = 0.0;
         }
     }
 }
